c62a34f3ff345de391b9635006d5af3ca44a4298
Kristijan Turić <kristijan.turic1@gmail.com>
2025-03-29 23:42:58 +0100

    Add basic UI design

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import "./style.css";
 import knightMoves from "./knightTravails";
 
+const chessboard = document.getElementById("chessboard");
+
+for (let row = 0; row < 8; row++) {
+    let tr = document.createElement("tr");
+    for (let col = 0; col < 8; col++ ) {
+        let td = document.createElement("td");
+        td.className = (row + col) % 2 === 0 ? "white" : "black";
+        tr.appendChild(td);
+    }
+    chessboard.appendChild(tr);
+}
+
 knightMoves([0, 0], [3, 3], 8);
